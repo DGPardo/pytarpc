1f76f3468a39fcf9b4712c68ca6289bf33bb8303
Diego Pardo <diego.pardo@validusrm.com>
2025-11-15 15:06:30 +0000

    Improved compatibility with Apple devices and updated dependencies

--- example/python_client/example_client.py
+++ example/python_client/example_client.py
@@ -1,14 +1,18 @@
-from rpc_model import PyRpcAPIClient
+from rpc_model import PyRpcAPIClient, CustomDataType
 import asyncio
 
 
 async def main():
     stub = await PyRpcAPIClient.connect("0.0.0.0:5000")
     response = await stub.hello("Diego")
-    print(response)
+    print(f'stub.hello("Diego"): {response}')
 
     response = await stub.sum_numbers(1, 2)
-    print(response)
+    print(f"stub.sum_numbers(1, 2): {response}")
+
+    foo = CustomDataType(field1="Diego", field2=[0.0, 1.0, 2.0])
+    response: CustomDataType = await stub.echo(foo)
+    print(f"stub.echo(CustomDataType(Diego, [0., 1., 2.])) = {response}")
 
 
 asyncio.run(main())
--- example/rpc-model/src/lib.rs
+++ example/rpc-model/src/lib.rs
@@ -4,5 +4,6 @@ pub mod model;
 #[pymodule]
 fn rpc_model(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<model::PyRpcAPIClient>()?;
+    m.add_class::<model::CustomDataType>()?;
     Ok(())
 }
